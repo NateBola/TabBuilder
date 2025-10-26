aa6de7e98386712bdea1db1a6dbdb18dbdf88653
Neythan <neythanpoling@gmail.com>
2025-10-25 23:33:55 -0400

    Added ability to draw tab lines

--- frames.py
+++ frames.py
@@ -2,6 +2,7 @@ import pygame as py
 from pygame import Surface
 from constants import *
 from dataclasses import dataclass
+from math import floor
 
 class Main():
     """Main window of the program, handles events, and all input"""
@@ -17,7 +18,7 @@ class Main():
         py.display.set_caption("TabSnake")
 
         # Class Variables
-        self.surface = py.display.set_mode((screen_width, screen_height))
+        self.surface = py.display.set_mode((screen_width, screen_height), flags=py.RESIZABLE)
         self.clock = py.time.Clock()
         self.draw_functions = list()
 
@@ -54,11 +55,42 @@ class Main():
         py.quit()
 
 class TabStaff():
+
     def __init__(self, master: Main):
         self.master = master
-        self.surface = Surface((600, 60))
 
-    def _draw_staff(self):
-        self.surface.fill(ATOMIC_TANGERINE)
-        self.master.surface.blit(self.surface, (0, 0))
+        # Tab Staff Settings
+        self.xpad = 20
+        self.ypad = 40
+        self.line_height = 10
+        self.line_xpad = 2
+
+        self.line_color = (255, 255, 255)
+        self.bg_color = DIM_GRAY
+
+    def draw_staff(self):
+        screen_width, screen_height = py.display.get_window_size()
+        tab_width = screen_width - (2 * self.xpad)
+        tab_height = self.line_height * 5
+        line_start = self.xpad + self.line_xpad
+        line_end = screen_width - self.xpad - self.line_xpad
+        rows = floor((screen_height - (2 * self.ypad)) / (tab_height + self.ypad))
+
+        tab_rect = py.rect.Rect(self.xpad, 0, tab_width, tab_height)
+
+        tab_yloc = self.ypad
+        for i in range(rows):
+            
+            tab_rect.top = tab_yloc
+            py.draw.rect(self.master.surface, self.bg_color, tab_rect)
+
+            line_yloc = tab_yloc + self.line_height/2
+            for i in range(5):
+                py.draw.line(self.master.surface, WHITE, (line_start, line_yloc), (line_end, line_yloc))
+                line_yloc += self.line_height
+
+            tab_yloc = tab_yloc + tab_height + self.ypad
+
+
+
 
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from frames import Main, TabStaff
 
 root = Main(screen_width=700, screen_height=500)
 tab = TabStaff(root)
-root.add_draw(tab._draw_staff)
+root.add_draw(tab.draw_staff)
 
 root.start()
