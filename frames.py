b0e208a186eaffc68d34f9d2b3be7cebfe4e3efa
Neythan <neythanpoling@gmail.com>
2025-10-25 22:02:31 -0400

    Commit to help keep track of progress

--- frames.py
+++ frames.py
@@ -6,6 +6,12 @@ from dataclasses import dataclass
 class Main():
     """Main window of the program, handles events, and all input"""
     def __init__(self, screen_width: int, screen_height: int):
+        """_summary_
+
+        Args:
+            screen_width (int): The width of the pygame window
+            screen_height (int): The height of the pygame window
+        """
         # Initalize
         py.init()
         py.display.set_caption("TabSnake")
@@ -13,6 +19,10 @@ class Main():
         # Class Variables
         self.surface = py.display.set_mode((screen_width, screen_height))
         self.clock = py.time.Clock()
+        self.draw_functions = list()
+
+    def add_draw(self, draw_function):
+        self.draw_functions.append(draw_function)
 
     def start(self):
         running = True
@@ -30,6 +40,10 @@ class Main():
         
             #clear the screen
             self.surface.fill(BLACK)
+
+            #draw functions
+            for func in self.draw_functions:
+                func()
         
             # flip() updates the screen to make our changes visible
             py.display.flip()
@@ -40,11 +54,11 @@ class Main():
         py.quit()
 
 class TabStaff():
-    def __init__(self, master):
-        super().__init__(master)
-        self.line_width = 2
-        self.line_spacing = 5
+    def __init__(self, master: Main):
+        self.master = master
+        self.surface = Surface((600, 60))
 
     def _draw_staff(self):
-        pass
+        self.surface.fill(ATOMIC_TANGERINE)
+        self.master.surface.blit(self.surface, (0, 0))
 
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame as py
 from constants import *
-from frames import Main
+from frames import Main, TabStaff
 
 root = Main(screen_width=700, screen_height=500)
+tab = TabStaff(root)
+root.add_draw(tab._draw_staff)
+
 root.start()
